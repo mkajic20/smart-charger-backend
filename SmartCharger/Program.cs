10bc4e727554ce1ae927ac673a06e342cd7c6cfe
lgaljer20 <lgaljer20@foi.hr>
2023-10-31 15:16:20 +0100

    Configured database connection

--- SmartCharger/Program.cs
+++ SmartCharger/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using SmartCharger.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,7 +9,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+builder.Services.AddDbContext<SmartChargerContext>(option => option.UseNpgsql(builder.Configuration.GetConnectionString("connection")));
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
