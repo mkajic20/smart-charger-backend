3c761b14315007a92cec81431a32923bfb9fcac9
Leon Raknić <lraknic20@student.foi.hr>
2023-11-01 10:15:17 +0100

    Added DTOs and IRegisterService
    Added DTO for request and response. Added interface for RegisterService.

---
+++ SmartCharger.Business/DTOs/RegisterResponseDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace SmartCharger.Business.DTOs
+{
+    public class RegisterResponseDTO
+    {
+        public bool Success { get; set; }
+        public string Message { get; set; }
+        public string? Error { get; set; }
+        public RegisterDTO? User { get; set; }
+    }
+}
