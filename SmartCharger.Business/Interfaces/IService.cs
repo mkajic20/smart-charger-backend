50b1e9afa13c9a166413cb99275837c7d279b4d1
Leon Raknić <lraknic20@student.foi.hr>
2023-11-01 09:15:37 +0100

    Created GenericService and IService

---
+++ SmartCharger.Business/Interfaces/IService.cs
@@ -0,0 +1,7 @@
+﻿namespace SmartCharger.Business.Interfaces
+{
+    public interface IService
+    {
+        Task DeleteAsync(int id);
+    }
+}
---
+++ SmartCharger.Business/Services/GenericService.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SmartCharger.Business.Interfaces;
+using SmartCharger.Data;
+using SmartCharger.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartCharger.Business.Services
+{
+    public class GenericService<TEntity> : IService where TEntity : BaseEntity
+    {
+        protected readonly SmartChargerContext _context;
+
+        public GenericService(SmartChargerContext context)
+        {
+            _context = context;
+        }
+
+        public async Task DeleteAsync(int id)
+        {
+            await _context.Set<TEntity>().Where(x => x.Id == id).ExecuteDeleteAsync();
+        }
+    }
+}
