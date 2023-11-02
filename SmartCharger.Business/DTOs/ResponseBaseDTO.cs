0d12fbfcfaa83b3d06ca15768e2be8e743838335
Leon Raknić <lraknic20@student.foi.hr>
2023-11-02 14:10:56 +0100

    Created DTOs and interface ILoginService
    Added one base response for reusability. Now RegisterResponseDTO and LoginResponseDTO inherit ResponseBaseDTO. Added interface for ILoginService.

---
+++ SmartCharger.Business/DTOs/ResponseBaseDTO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace SmartCharger.Business.DTOs
+{
+    public class ResponseBaseDTO
+    {
+        public bool Success { get; set; }
+        public string Message { get; set; }
+        
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+        public string? Error { get; set; }
+    }
+}
