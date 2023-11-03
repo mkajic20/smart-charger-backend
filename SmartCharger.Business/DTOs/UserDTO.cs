70d5ab93ead2b44b56f40face652bdbf069fc824
lgaljer20 <lgaljer20@foi.hr>
2023-11-03 11:08:01 +0100

    Added BaseDTO and extended UserDTO

--- SmartCharger.Business/DTOs/UserDTO.cs
+++ SmartCharger.Business/DTOs/UserDTO.cs
@@ -1,17 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Text.Json.Serialization;
-using System.Threading.Tasks;
-
+﻿using System.Text.Json.Serialization;
 namespace SmartCharger.Business.DTOs
 {
-    public class UserDTO
+    public class UserDTO : BaseDTO
     {
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Email { get; set; }
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+        public bool? Active { get; set; }
 
         [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
         public string? Password { get; set; }
