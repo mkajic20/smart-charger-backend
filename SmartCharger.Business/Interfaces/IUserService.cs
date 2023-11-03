2094f95adc6e9ca289d26728e57bab9489beb21b
lgaljer20 <lgaljer20@foi.hr>
2023-11-03 11:29:46 +0100

    Implemented IUserService and UserService Main methods in services are: GetAllUsers, GetUserById, UpdateActiveStatus and UpdateRole

---
+++ SmartCharger.Business/Interfaces/IUserService.cs
@@ -0,0 +1,13 @@
+﻿IUserService
+using SmartCharger.Business.DTOs;
+
+namespace SmartCharger.Business.Interfaces
+{
+    public interface IUserService
+    {
+        Task<UsersResponseDTO> GetAllUsers();
+        Task<SingleUserResponseDTO> GetUserById(int userId);
+        Task<SingleUserResponseDTO> UpdateRole(int userId);
+        Task<SingleUserResponseDTO> UpdateActiveStatus(int userId);
+    }
+}
