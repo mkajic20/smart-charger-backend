2598973812e4f739856945214fd69563c72c8111
lgaljer20 <lgaljer20@foi.hr>
2023-11-03 11:10:01 +0100

    Added two new response: UsersResponseDTO and SingleUserResponseDTO Both responses will be used for user managament SingleUserResponse will be used when modifying single User's attributes and UsersResponseDTO will be used in GetAll method

---
+++ SmartCharger.Business/DTOs/UsersResponseDTO.cs
@@ -0,0 +1,12 @@
+﻿using System.Text.Json.Serialization;
+
+namespace SmartCharger.Business.DTOs
+{
+    public class UsersResponseDTO : ResponseBaseDTO
+    {
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+        public List<UserDTO>? Users { get; set; }
+    }
+}
+
+
