b81df56c21fb2d9dc25110091e29ea1a18bc9481
lgaljer20 <lgaljer20@foi.hr>
2023-11-14 20:28:34 +0100

    SC-48 Created simple ChargerDTO

---
+++ SmartCharger.Business/DTOs/ChargerDTO.cs
@@ -0,0 +1,17 @@
+﻿using System.Text.Json.Serialization;
+
+namespace SmartCharger.Business.DTOs
+{
+    public class ChargerDTO : BaseDTO
+    {
+        public string Name { get; set; }
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+        public DateTime CreationTime { get; set; }
+
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+        public DateTime? LastSync { get; set; }
+        public bool Active { get; set; }
+        public int CreatorId { get; set; }
+    }
+}
