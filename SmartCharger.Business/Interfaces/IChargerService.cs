17f99cdbef95ada47844a91b9fe451bc9e337394
lgaljer20 <lgaljer20@foi.hr>
2023-11-14 20:37:44 +0100

    SC-49 Created interface for ChargerService

---
+++ SmartCharger.Business/Interfaces/IChargerService.cs
@@ -0,0 +1,13 @@
+﻿using SmartCharger.Business.DTOs;
+
+namespace SmartCharger.Business.Interfaces
+{
+    public interface IChargerService
+    {
+        Task<ChargerResponseDTO> CreateNewCharger();
+        Task<ChargerResponseDTO> GetAllChargers();
+        Task<ChargerResponseDTO> UpdateCharger(int chargerId);
+        Task<ChargerResponseDTO> DeleteCharger(int chargerId);
+
+    }
+}
