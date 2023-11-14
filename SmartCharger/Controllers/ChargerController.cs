ee23822d951c50ff0694952fae91ef7afe1a8c44
lgaljer20 <lgaljer20@foi.hr>
2023-11-14 20:48:22 +0100

    SC-51 Created ChargerController Made initial empty charger for handling admin-specific charger functionalities

---
+++ SmartCharger/Controllers/ChargerController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SmartCharger.Business.Interfaces;
+
+namespace SmartCharger.Controllers
+{
+    [Route("api/admin/")]
+    [ApiController]
+    public class ChargerController : ControllerBase
+    {
+        private readonly IChargerService _chargerService;
+
+        public ChargerController(IChargerService chargerService)
+        {
+            _chargerService = chargerService;
+        }
+    }
+}
