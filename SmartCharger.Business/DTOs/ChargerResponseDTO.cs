c270d7393563165d32395c41241207ab50fe6a9b
lgaljer20 <lgaljer20@foi.hr>
2023-11-14 20:34:08 +0100

    SC-52 Created ChargerResponseDTO

---
+++ SmartCharger.Business/DTOs/ChargerResponseDTO.cs
@@ -0,0 +1,11 @@
+﻿
+using System.Text.Json.Serialization;
+
+namespace SmartCharger.Business.DTOs
+{
+    public class ChargerResponseDTO : ResponseBaseDTO
+    {
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+        public List<ChargerDTO>? Chargers { get; set; }
+    }
+}
