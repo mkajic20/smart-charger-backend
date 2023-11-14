028efbf1b34e374f73c2cc5ec61e442d14194aef
lgaljer20 <lgaljer20@foi.hr>
2023-11-14 20:45:32 +0100

    SC-50 Created ChargerService Methods are currently empty.

---
+++ SmartCharger.Business/Services/ChargerService.cs
@@ -0,0 +1,34 @@
+﻿using SmartCharger.Business.DTOs;
+using SmartCharger.Business.Interfaces;
+using SmartCharger.Data;
+using SmartCharger.Data.Entities;
+
+namespace SmartCharger.Business.Services
+{
+    public class ChargerService : GenericService<Charger>, IChargerService
+    {
+        public ChargerService(SmartChargerContext context) : base(context)
+        {
+        }
+
+        public async Task<ChargerResponseDTO> CreateNewCharger()
+        {
+            throw new NotImplementedException();
+        }
+
+        public  async Task<ChargerResponseDTO> DeleteCharger(int chargerId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async  Task<ChargerResponseDTO> GetAllChargers()
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<ChargerResponseDTO> UpdateCharger(int chargerId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
