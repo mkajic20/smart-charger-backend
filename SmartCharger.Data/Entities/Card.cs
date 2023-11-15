e1855f8409e1972f00347a9135b505c63eb3d42d
Leon Raknić <lraknic20@student.foi.hr>
2023-11-15 09:04:40 +0100

    SC-79 Added name attribute to card entity
    Added name attribute to card entity and added new migration.

--- SmartCharger.Data/Entities/Card.cs
+++ SmartCharger.Data/Entities/Card.cs
@@ -6,6 +6,7 @@ namespace SmartCharger.Data.Entities
     {
         public string Value { get; set; }
         public bool Active { get; set; }
+        public string Name { get; set; }
 
         [ForeignKey(nameof(User))]
         public int UserId { get; set; }
---
+++ SmartCharger.Data/Migrations/20231115080038_addedNameToCard.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SmartCharger.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class addedNameToCard : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Name",
+                table: "Cards",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Name",
+                table: "Cards");
+        }
+    }
+}
