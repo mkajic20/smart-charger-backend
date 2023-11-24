2158aaf551d65d47bc37b45a17c7552c0a3254f6
lgaljer20 <lgaljer20@foi.hr>
2023-11-24 12:09:05 +0100

    SC-102 Created EventResponseDTO

---
+++ SmartCharger.Business/DTOs/EventResponseDTO.cs
@@ -0,0 +1,10 @@
+﻿using System.Text.Json.Serialization;
+
+namespace SmartCharger.Business.DTOs
+{
+    public class EventResponseDTO : ResponseBaseDTO
+    {
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+        public List<EventDTO>? Events { get; set; }
+    }
+}
