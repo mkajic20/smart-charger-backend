3bec44cebf81df654b880c046f30cf7ed1550917
lgaljer20 <lgaljer20@foi.hr>
2023-11-24 16:47:06 +0100

    SC-105 Created EventController

---
+++ SmartCharger/Controllers/EventController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SmartCharger.Business.Interfaces;
+
+namespace SmartCharger.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EventController : ControllerBase
+    {
+
+        private readonly IEventService _eventService;
+
+        public EventController(IEventService eventService)
+        {
+            _eventService = eventService;
+        }
+    }
+}
