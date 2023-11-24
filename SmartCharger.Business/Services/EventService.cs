5e80c2c419e7751387dde8c7566748d48d97190a
lgaljer20 <lgaljer20@foi.hr>
2023-11-24 12:35:07 +0100

    SC-104 Created EventService

---
+++ SmartCharger.Business/Services/EventService.cs
@@ -0,0 +1,19 @@
+﻿using SmartCharger.Business.DTOs;
+using SmartCharger.Business.Interfaces;
+using SmartCharger.Data;
+using SmartCharger.Data.Entities;
+
+namespace SmartCharger.Business.Services
+{
+    public class EventService : GenericService<Event>, IEventService
+    {
+        public EventService(SmartChargerContext context) : base(context)
+        {
+        }
+
+        public Task<EventResponseDTO> GetUsersChargingHistory(int userId, int page = 1, int pageSize = 5, string search = null)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
