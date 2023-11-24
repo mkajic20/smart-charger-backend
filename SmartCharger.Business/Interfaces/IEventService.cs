3b57584e2ee8a089e53f43a7bc52395626fa6262
lgaljer20 <lgaljer20@foi.hr>
2023-11-24 12:12:01 +0100

    SC-103 Created interface for EventService

---
+++ SmartCharger.Business/Interfaces/IEventService.cs
@@ -0,0 +1,9 @@
+﻿using SmartCharger.Business.DTOs;
+
+namespace SmartCharger.Business.Interfaces
+{
+    public interface IEventService
+    {
+        Task<EventResponseDTO> GetUsersChargingHistory(int userId, int page, int pageSize, string search);
+    }
+}
