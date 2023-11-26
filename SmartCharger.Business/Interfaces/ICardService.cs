08168afd6bb738c0d424815aa67601c5d4d777a5
Leon Raknić <lraknic20@student.foi.hr>
2023-11-26 11:02:21 +0100

    SC-93 Added methods in ICardService
    Added methods GetAllCardsForUser, GetCardByIdForUser, AddCard and DeleteCardForUser.

--- SmartCharger.Business/Interfaces/ICardService.cs
+++ SmartCharger.Business/Interfaces/ICardService.cs
@@ -4,9 +4,15 @@ namespace SmartCharger.Business.Interfaces
 {
     public interface ICardService
     {
+        // Admin
         Task<CardsResponseDTO> GetAllCards(int page, int pageSize, string search);
         Task<CardsResponseDTO> GetCardById(int cardId);
         Task<CardsResponseDTO> UpdateActiveStatus(int cardId);
         Task<CardsResponseDTO> DeleteCard(int cardId);
+        // Customer
+        Task<CardsResponseDTO> GetAllCardsForUser(int userId);
+        Task<CardsResponseDTO> GetCardByIdForUser(int cardId, int userId);
+        Task<CardsResponseDTO> AddCard(CardDTO card);
+        Task<CardsResponseDTO> DeleteCardForUser(int cardId, int userId);
     }
 }
