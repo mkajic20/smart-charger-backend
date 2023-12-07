efe722edecbf7441f425152201d72b1432a3a728
lgaljer20 <lgaljer20@foi.hr>
2023-12-07 23:49:52 +0100

    SC-172 Created simple RoleController

---
+++ SmartCharger/Controllers/RoleController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SmartCharger.Business.DTOs;
+using SmartCharger.Business.Interfaces;
+using SmartCharger.Business.Services;
+
+namespace SmartCharger.Controllers
+{
+    [Route("api/")]
+    [ApiController]
+    public class RoleController : ControllerBase
+    {
+        private readonly IRoleService _roleService;
+
+        public RoleController(IRoleService roleService)
+        {
+            _roleService = roleService;
+        }
+    }
+}
