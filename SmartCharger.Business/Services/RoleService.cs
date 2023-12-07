7e3ab033d6fad4b03e045dd68e9ed4f4329abb4a
lgaljer20 <lgaljer20@foi.hr>
2023-12-07 23:41:38 +0100

    SC-171 Created RoleService GetAllRoles is currently throwing NotImplementedException

---
+++ SmartCharger.Business/Services/RoleService.cs
@@ -0,0 +1,20 @@
+﻿
+using SmartCharger.Business.DTOs;
+using SmartCharger.Business.Interfaces;
+using SmartCharger.Data;
+using SmartCharger.Data.Entities;
+
+namespace SmartCharger.Business.Services
+{
+    public class RoleService : GenericService<Role>,IRoleService
+    {
+        public RoleService(SmartChargerContext context) : base(context)
+        {
+        }
+
+        public async Task<RoleResponseDTO> GetAllRoles()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
