5b5f99781eaadeea0e2c74854fa68e97e8ea480b
lgaljer20 <lgaljer20@foi.hr>
2023-12-20 17:44:25 +0100

    SC-183 Created GoogleUserDTO to represent user's Google profile information.

---
+++ SmartCharger.Business/DTOs/GoogleUserDTO.cs
@@ -0,0 +1,10 @@
+﻿
+namespace SmartCharger.Business.DTOs
+{
+    public class GoogleUserDTO
+    {
+        public string Email { get; set; }
+        public string Name { get; set; }
+        public string LastName { get; set; }
+    }
+}
