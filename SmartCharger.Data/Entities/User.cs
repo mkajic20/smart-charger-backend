6d82784f5c0a56bf2e75d9fff4ae58c8c7bcb3fc
lgaljer20 <lgaljer20@foi.hr>
2023-12-21 13:06:58 +0100

    SC-184 Made user's password and salt nullable Login via google doesn't provide password.

--- SmartCharger.Data/Entities/User.cs
+++ SmartCharger.Data/Entities/User.cs
@@ -7,10 +7,10 @@ namespace SmartCharger.Data.Entities
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Email { get; set; }
-        public string Password { get; set; }
+        public string? Password { get; set; }
         public bool? Active { get; set; }
         public DateTime CreationTime { get; set; }
-        public string Salt { get; set; }
+        public string? Salt { get; set; }
 
         [ForeignKey(nameof(Role))]
         public int RoleId { get; set; }
