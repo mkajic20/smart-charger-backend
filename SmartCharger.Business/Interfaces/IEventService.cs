121a5e32cd4a11ad8d415f80131ce05312819cbc
lgaljer20 <lgaljer20@foi.hr>
2024-01-05 13:49:59 +0100

    SC-218  Added method GetFullChargingHistory

--- SmartCharger.Business/Interfaces/IEventService.cs
+++ SmartCharger.Business/Interfaces/IEventService.cs
@@ -5,6 +5,7 @@ namespace SmartCharger.Business.Interfaces
     public interface IEventService
     {
         Task<EventResponseDTO> GetUsersChargingHistory(int userId, int page, int pageSize, string search);
+        Task<EventResponseDTO> GetFullChargingHistory(int page, int pageSize, string search);
         Task<EventResponseDTO> StartCharging(EventChargingDTO eventDTO);
         Task<EventResponseDTO> EndCharging(EventChargingDTO eventDTO);
     }
